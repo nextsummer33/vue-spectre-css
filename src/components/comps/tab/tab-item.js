b1641223273e76f73e53ef2b3ab4814edb7a764e
Kuan Cheang <kuancheang@fridaycreation.com>
2019-07-26 15:31:20 +0800

    added action attribute into tab-item.

--- src/components/comps/tab/tab-item.js
+++ src/components/comps/tab/tab-item.js
@@ -1,14 +1,17 @@
-import { strType } from '@/utils/proptypes';
+import { strType, boolType } from '@/utils/proptypes';
 import TabLink from './tab-link';
 
 export default {
   functional: true,
   props: {
-    tag: strType('li')
+    tag: strType('li'),
+    action: boolType()
   },
   render(h, { props, data, children }) {
-    return h(props.tag, { staticClass: 'tab-item' }, [
-      h(TabLink, data, children)
-    ]);
+    return h(
+      props.tag,
+      { staticClass: 'tab-item', class: { 'tab-action': props.action } },
+      props.action ? children : [h(TabLink, data, children)]
+    );
   }
 };
--- src/views/Tab.vue
+++ src/views/Tab.vue
@@ -5,14 +5,24 @@
       <s-tab-item>Playlist</s-tab-item>
     </s-tab>
 
-    <s-tab block>
-      <s-tab-item href="#music" active badge>Music</s-tab-item>
+    <s-tab>
+      <s-tab-item href="#music" active badge>
+        Active
+        <s-btn tag="span" clear />
+      </s-tab-item>
       <s-tab-item badge="2">Playlist</s-tab-item>
+      <s-tab-item action>
+        <s-input-grp inline>
+          <s-input sm />
+          <s-btn sm primary input-grp>Search</s-btn>
+        </s-input-grp>
+      </s-tab-item>
     </s-tab>
     <s-tab :items="items" v-model="tabIndex"/>
     <s-tab :items="items" @select="tabIndex = $event"/>
+    <s-tab block :items="items" @select="tabIndex = $event"/>
     <div id="basketball" v-show="tabIndex == 0">basketball</div>
-    <div id="football" v-show="tabIndex == 1">badminton</div>
+    <div id="football" v-show="tabIndex == 1">football</div>
     <div id="badminton" v-show="tabIndex == 2">Badminton</div>
   </s-grid>
 </template>
